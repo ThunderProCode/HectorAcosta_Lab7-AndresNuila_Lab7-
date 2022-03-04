c0d55d624cfbe0398cee199fb3ed7cddf1075c15
ThunderKnight <hector889ju@gmail.com>
2022-03-04 13:41:06 -0600

    Mainscreen design done

---
+++ src/MainPackage/Equipo.java
@@ -0,0 +1,196 @@
+package MainPackage;
+
+public class Equipo {
+    
+    private String name;
+    private int pj;
+    private int pg;    
+    private int pe;
+    private int pp;
+    private int gf;
+    private int gc;
+    private int dg;
+    private int pts;
+
+    public Equipo(String name, int pj, int pg, int pe, int pp, int gf, int gc, int dg, int pts) {
+        this.name = name;
+        this.pj = pj;
+        this.pg = pg;
+        this.pe = pe;
+        this.pp = pp;
+        this.gf = gf;
+        this.gc = gc;
+        this.dg = dg;
+        this.pts = pts;
+    }
+    
+    /**
+     * Get the value of pg
+     *
+     * @return the value of pg
+     */
+    public int getPg() {
+        return pg;
+    }
+
+    /**
+     * Set the value of pg
+     *
+     * @param pg new value of pg
+     */
+    public void setPg(int pg) {
+        this.pg = pg;
+    }
+
+    
+    /**
+     * Get the value of dg
+     *
+     * @return the value of dg
+     */
+    public int getDg() {
+        return dg;
+    }
+
+    /**
+     * Set the value of dg
+     *
+     * @param dg new value of dg
+     */
+    public void setDg(int dg) {
+        this.dg = dg;
+    }
+
+    
+    /**
+     * Get the value of pp
+     *
+     * @return the value of pp
+     */
+    public int getPp() {
+        return pp;
+    }
+
+    /**
+     * Set the value of pp
+     *
+     * @param pp new value of pp
+     */
+    public void setPp(int pp) {
+        this.pp = pp;
+    }
+
+    
+    /**
+     * Get the value of pe
+     *
+     * @return the value of pe
+     */
+    public int getPe() {
+        return pe;
+    }
+
+    /**
+     * Set the value of pe
+     *
+     * @param pe new value of pe
+     */
+    public void setPe(int pe) {
+        this.pe = pe;
+    }
+
+    
+    
+    /**
+     * Get the value of pj
+     *
+     * @return the value of pj
+     */
+    public int getPj() {
+        return pj;
+    }
+
+    /**
+     * Set the value of pj
+     *
+     * @param pj new value of pj
+     */
+    public void setPj(int pj) {
+        this.pj = pj;
+    }
+
+    
+    /**
+     * Get the value of pts
+     *
+     * @return the value of pts
+     */
+    public int getPts() {
+        return pts;
+    }
+
+    /**
+     * Set the value of pts
+     *
+     * @param pts new value of pts
+     */
+    public void setPts(int pts) {
+        this.pts = pts;
+    }
+
+    
+    /**
+     * Get the value of gc
+     *
+     * @return the value of gc
+     */
+    public int getGc() {
+        return gc;
+    }
+
+    /**
+     * Set the value of gc
+     *
+     * @param gc new value of gc
+     */
+    public void setGc(int gc) {
+        this.gc = gc;
+    }
+
+    
+    /**
+     * Get the value of gf
+     *
+     * @return the value of gf
+     */
+    public int getGf() {
+        return gf;
+    }
+
+    
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + ";" + pj + ";" + pg + ";" + pe + ";" + pp + ";" + gf + ";" + gc + ";" + dg + ";" + pts;
+    }
+
+    
+    
+}
--- src/MainPackage/MainScreen.java
+++ src/MainPackage/MainScreen.java
@@ -27,17 +27,51 @@ public class MainScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre", "Partidos Ganados", "Partidos Empatados", "Partidos Perdidos", "Goles Favor", "Goles Contra", "Diferencia", "Pts"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Object.class, java.lang.Integer.class, java.lang.Integer.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
+
+        jMenu1.setText("Equipo");
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 950, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 295, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -79,5 +113,9 @@ public class MainScreen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
